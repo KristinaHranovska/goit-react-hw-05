0cd3f77f53f85ab56e02a34b5acdaef4a181dad1
Kristina <kr.hranovska@gmail.com>
2024-03-05 22:51:04 +0200

    add canvas animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "../node_modules/modern-normalize/modern-normalize.css";
 
-function App() {
-  return <></>;
-}
+import { useEffect, useRef } from "react";
+import webGLFluidEnhanced from "webgl-fluid-enhanced";
+
+const App = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    webGLFluidEnhanced.simulation(canvasRef.current, {
+      SIM_RESOLUTION: 256,
+      DENSITY_DISSIPATION: 0.8,
+      PRESSURE_ITERATIONS: 30,
+      COLOR_PALETTE: ["#61dafb", "#a8dadc", "#457b9d", "#1d3557", "#f1faee"],
+    });
+  }, []);
+
+  return <canvas ref={canvasRef} style={{ width: "100vw", height: "100vh" }} />;
+};
 
 export default App;
