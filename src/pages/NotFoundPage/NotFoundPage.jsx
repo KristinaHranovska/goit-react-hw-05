0cd71000dabff4fa039eb899a473011046855a0c
Kristina <kr.hranovska@gmail.com>
2024-03-08 21:19:27 +0200

    not found page is ready

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -5,20 +5,24 @@ const NotFoundPage = () => {
   const location = useLocation();
   const backLinkHref = location.state?.from ?? "/";
   return (
-    <section className={style.notFound}>
-      <h1 className={style.notFoundTitle}>404</h1>
-      <h2 className={style.notFoundTitleInform}>Page not found</h2>
-      <img
-        className={style.notFoundImg}
-        src="/src/img/404-page.png"
-        alt="404-page"
-      />
-      <p className={style.notFoundMessage}>
-        We`re sorry, the page you requesterd could not be found
-      </p>
-      <Link to={backLinkHref}>
-        <button className={style.notFoundBtn}>Go to homepage</button>
-      </Link>
+    <section className={style.containerNotFound}>
+      <div className={style.notFound}>
+        <h1 className={style.notFoundTitle}>404</h1>
+        <img
+          className={style.notFoundImg}
+          src="/src/img/404-page.png"
+          alt="404-page"
+          width="200"
+        />
+        <h2 className={style.notFoundTitleInform}>Page not found</h2>
+
+        <p className={style.notFoundMessage}>
+          We`re sorry, the page you requesterd could not be found
+        </p>
+        <Link to={backLinkHref}>
+          <button className={style.notFoundBtn}>Go to homepage</button>
+        </Link>
+      </div>
     </section>
   );
 };
