d0254715e10c07abb0ca8382e688c13f12604eb7
Kristina <kr.hranovska@gmail.com>
2024-03-10 16:53:26 +0200

    Update MovieCastItem.jsx

--- src/components/MovieCastItem/MovieCastItem.jsx
+++ src/components/MovieCastItem/MovieCastItem.jsx
@@ -2,15 +2,16 @@ import style from "./MovieCastItem.module.css";
 
 const MovieCastItem = ({ dataCast: { profile_path, name, character } }) => {
   const urlImg = `https://image.tmdb.org/t/p/w500/${profile_path}`;
-  const fallbackImage = {
-    image:
-      "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg",
-  };
+  // const fallbackImage = {
+  //   image:
+  //     "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg",
+  // };
   return (
     <div>
       <img
         className={style.castImg}
-        src={profile_path ? urlImg : fallbackImage.image}
+        // src={profile_path ? urlImg : fallbackImage.image}
+        src={urlImg}
         alt={name}
         width="200"
         height="300"
